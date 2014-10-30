7889787f97c5801b1a7b037912722d623fe22d70
Richard Zhan <zhan97@gmail.com>
2014-10-29 20:46:41 -0700

    Added more html and forms

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request
-
+import pymongo
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def home():
+    """
+    if request.method == "POST":
+        form = request.form
+        if form["submit"] == 'yes' and form['user'] != "" and form["pass"] != "":
+            #mongostuff
+    """
     return render_template("home.html")
-    return "hi"
+
+@app.route("/register",methods=["POST","GET"])
+def register():
+    message = ""
+    if request.method == "POST":
+        form = request.form
+        if form["submit"] == 'yes' and form['user'] != "" and form["pass"] != "":
+            #mongostuff
+            message = "Successfully Registered!"
+    return render_template("register.html",m=message)
 
 if __name__ == "__main__":
     app.debug = True
