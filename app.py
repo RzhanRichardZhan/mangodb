3661a2c64eca3c3517b350065f87c59ff980510f
Richard Zhan <zhan97@gmail.com>
2014-10-31 19:16:23 -0700

    Sessions Working and More Mango

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pymongo import Connection
 
 
 app = Flask(__name__)
+app.secret_key='{\xdft\xb7\x06f\x9b\xa4\x0eP\xe1n\xdd\xd4\x93\x01\xd3`\xc1\xe5\xc1|\x0e`'
 
 
 
@@ -21,6 +22,7 @@ def home():
                     if person['pass']==form['pass']:
                         worked = True                        
         if worked:
+            session['username']=form['user']
             return redirect(url_for('account'))
         else:
             return render_template("home.html",text = "Incorrect password or username")
@@ -43,30 +45,38 @@ def register():
                 message = "Passwords Are Too Short!"
                 color = "red"
             else:
-                db.info.insert({'user':form['user'],'pass':form['pass']})
-                message = "Successfully Registered!"
+                if db.info.find_one({'user':form['user']}) == None:
+                    db.info.insert({'user':form['user'],'pass':form['pass']})
+                    message = "Successfully Registered!"
+                else:
+                    message = "Username Already Registered!"
+                    color = "red"
     return render_template("register.html",m=message, c=color)
 
 @app.route("/account")
 def account():
-    """
-    if !('username' in session):
+    
+    if not ('username' in session):
        return "STOP TRYIN TO CHEAT THE MANGO STORE"
-    """
-    return render_template("account.html")
-    return "HEY ITS THE ACCOUNT PAGE"
+    
+    return render_template("account.html",u=session['username'])
 
 @app.route("/logout")
 def logout():
     #CLEAR ALL COOKIES
+    session.pop("username",None)
     return redirect(url_for("home"))
 
 @app.route("/about")
 def about():
     return render_template("about.html")
-    return "HEY ITS THE ABOUT PAGE"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 if __name__ == "__main__":
+    
     conn = Connection()
     db = conn['ayrz']
     app.debug = True
