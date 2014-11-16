ac84948e7a56be6b82c0bf17026d58be2e5b7153
Richard Zhan <zhan97@gmail.com>
2014-11-16 14:20:58 -0800

    Done?

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, abort, session, escape, flash
 from pymongo import Connection
+from functools import wraps
 
 
 app = Flask(__name__)
 app.secret_key='{\xdft\xb7\x06f\x9b\xa4\x0eP\xe1n\xdd\xd4\x93\x01\xd3`\xc1\xe5\xc1|\x0e`'
 
-def authenticate(f):
+def authenticater(f):
+    @wraps(f)
     def inner():
-        if username in session:
-            return f:
+        if 'username' in session:
+            return f()
         else:
-            return redirect(url_for('register'))
+            return redirect('/register')
     return inner
 
 @app.route("/", methods=["POST","GET"])
@@ -58,8 +60,9 @@ def register():
                     message = "Username Already Registered!"
                     color = "red"
     return render_template("register.html",m=message, c=color)
-@authenticate
+
 @app.route("/account", methods=["POST","GET"])
+@authenticater
 def account():
     number = db.info.find_one({"user":session["username"]})["mango"]
     
@@ -71,26 +74,29 @@ def account():
             number = number + 1
         
     return render_template("account.html",u=session['username'], number = number)
-    
-@authenticate
+
+
 @app.route("/leader")
+@authenticater
 def leader():
     a = db.info.find()
     data = [ (x["user"],x["mango"]) for x in a]
     return render_template("leader.html",data = data, u = session["username"])
     
-@authenticate
+
 @app.route("/logout")
+@authenticater
 def logout():
     #this counts as a page right
     session.pop("username",None)
     return redirect(url_for("home"))
 
-@authenticate
+
 @app.route("/about")
 def about():
     text = ""
-    text = "You are logged in " + session["username"]
+    if 'username' in session:
+        text = "You are logged in " + session["username"]#didn't want to authenticate this since it's a specific case. If there were more cases, prob make a new wrapper so i can factor
     return render_template("about.html",text=text)
 
 @app.errorhandler(404)
